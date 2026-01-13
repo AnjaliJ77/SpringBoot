5be80a4f546ca970065af48cf9b4f38823790e9e
AnjaliJ77 <djanjali77duv@gmail.com>
2026-01-13 14:43:11 +0530

    Update MySql.java

--- SpringBootAnno/ConditionalOnProperty Annotations/MySql.java
+++ SpringBootAnno/ConditionalOnProperty Annotations/MySql.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(prefix = "mysql",value = "enabled", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(prefix = "mysql",value = "enabled", havingValue = "true", matchIfMissing = false)        //Check app.property for the value and prefix
 public class MySql {
 
 	public MySql() {
@@ -13,3 +13,4 @@ public class MySql {
 	
 
 }
+
